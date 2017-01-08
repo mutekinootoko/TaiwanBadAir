b69b2ffe9be6fd9d55511d20ff75d57e77495108
Marku, Hao-Yu Lee <mutekinootoko@gmail.com>
2017-01-08 16:02:24 +0800

    condition AQI <= 30 is actually way too strict (according to the website), changed to 48

--- taiwanBadAirIfttt.py
+++ taiwanBadAirIfttt.py
@@ -18,7 +18,7 @@ for eachArea in areas :
 		jtitle = json.loads(eachArea['jtitle'])
 		#<area shape='rect' class='jTip' title='永和' alt='永和' jTitle='{"SiteName":"永和", "SiteKey":"Yonghe", "AreaKey":"North", "AQI":"", "AQIStyle":"AQI0", "MainPollutant":"","MainPollutantKey":"","PM10":"","PM25":"","O3":"","SO2":"","CO":"", "NO2":"","X":112,"Y":183,"O3_8":"","PM10_AVG":"","PM25_AVG":"","MonobjName":"交通"}' coords='112,183,120,191' href=javascript:showInfo('Yonghe'); id='Yonghe' />
 		# bad air condition 
-		if jtitle[u'SiteKey'] in locations and int(jtitle[u'AQI']) > 30:
+		if jtitle[u'SiteKey'] in locations and int(jtitle[u'AQI']) > 48:
 			warning += '{} , AQI:{} , PM2.5:{}, PM10:{}. '.format(jtitle[u'SiteKey'], jtitle[u'AQI'], jtitle[u'PM25'], jtitle[u'PM10'])
 print warning
 c = pycurl.Curl()
